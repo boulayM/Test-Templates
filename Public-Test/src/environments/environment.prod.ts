37e7a584cd775fc1357c5d67f3ead35a43d3bd4d
Boulay.M <macboulay2@gmail.com>
2026-02-17 15:43:05 +0100

    ApiUrl

--- Public-Test/src/environments/environment.prod.ts
+++ Public-Test/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://mabdev.alwaysdata.net/api-postgres/api',
   showSanityLogs: false, // logs desactivees en prod
   captchaEnabled: true,
   captchaSiteKey: '',
